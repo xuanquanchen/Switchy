f3cd989222f2e129a1c4c11fdbc2fe7894e8e2eb
Yiming Chen <yikming.chen@outlook.com>
2024-10-31 18:06:11 -0400

    Add detection to the previous IME

--- Switchy/main.c
+++ Switchy/main.c
@@ -1,7 +1,16 @@
 #include <Windows.h>
+#include <stdio.h>
+#include <imm.h>
+#pragma comment(lib, "Imm32.lib")
+
 #if _DEBUG
 #include <stdio.h>
 #endif // _DEBUG
+
+//define flag for switching subtypes
+#define SIMPLIFIED_CHINESE_SWITCH_METHOD_SHIFT 1
+#define SIMPLIFIED_CHINESE_SWITCH_METHOD_CTRL 2
+#define SIMPLIFIED_CHINESE_SWITCH_METHOD_CTRL_SPACE 3
 
 typedef NTSTATUS(WINAPI* RtlGetVersionPtr)(PRTL_OSVERSIONINFOW);
 
@@ -15,6 +24,7 @@ void PressKey(int keyCode);
 void ReleaseKey(int keyCode);
 void ToggleCapsLockState();
 LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam);
+void EnsureLanguageMode(int simplified_chinese_flag);
 
 
 HHOOK hHook;
@@ -49,7 +59,11 @@ int main(int argc, char** argv)
 		return 1;
 	}
 
+	// Set a global low-level keyboard hook. 
+	//WH_KEYBOARD_LL allows the hook to be in the current process without requiring a DLL,
+	//making it possible to capture keyboard events system-wide. (Low level hook don't need to inject into a DLL)
 	hHook = SetWindowsHookEx(WH_KEYBOARD_LL, LowLevelKeyboardProc, 0, 0);
+
 	if (hHook == NULL)
 	{
 		ShowError("Error calling \"SetWindowsHookEx(...)\"");
@@ -116,14 +130,14 @@ void ToggleCapsLockState()
 #endif // _DEBUG
 }
 
-
+//nCode is the situation of the hook, wParam is the event type now, lParam is a pointer pointing to KBDLLHOOKSTRUCT
 LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
 	KBDLLHOOKSTRUCT* key = (KBDLLHOOKSTRUCT*)lParam;
-	if (nCode == HC_ACTION && !(key->flags & LLKHF_INJECTED))
+	if (nCode == HC_ACTION && !(key->flags & LLKHF_INJECTED)) //make sure event is valid and not injected by other program(make sure just perform event on physical input)
 	{
 #if _DEBUG
-		const char* keyStatus = (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN) ? "pressed" : "released";
+		const char* keyStatus = (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN) ? "pressed" : "released"; //SYSJEYDOWN posted to the window with the keyboard focus when the user presses the F10 key (which activates the menu bar) or holds down the ALT key and then presses another key.
 		printf("Key %d has been %s\n", key->vkCode, keyStatus);
 #endif // _DEBUG
 		if (key->vkCode == VK_CAPITAL)
@@ -137,7 +151,7 @@ LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 #endif // _DEBUG
 				return 1;
 			}
-
+			
 			if (wParam == WM_KEYUP || wParam == WM_SYSKEYUP)
 			{
 				keystrokeCapsProcessed = FALSE;
@@ -152,14 +166,17 @@ LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 				{
 					if (!keystrokeShiftProcessed)
 					{
-						PressKey(VK_MENU);
+						//Caps Lock switch logic
+						PressKey(VK_MENU); //Alt KEY
 						PressKey(VK_LSHIFT);
 						ReleaseKey(VK_MENU);
 						ReleaseKey(VK_LSHIFT);
+
+						EnsureLanguageMode(SIMPLIFIED_CHINESE_SWITCH_METHOD_CTRL_SPACE);
 					}
 					else
 					{
-						keystrokeShiftProcessed = FALSE;
+						keystrokeShiftProcessed = FALSE; //reset situation
 					}
 				}
 			}
@@ -173,7 +190,7 @@ LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 			{
 				keystrokeCapsProcessed = TRUE;
 
-				if (keystrokeShiftProcessed == TRUE)
+				if (keystrokeShiftProcessed == TRUE) //We set shift first
 				{
 					ToggleCapsLockState();
 					return 1;
@@ -186,6 +203,8 @@ LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 						PressKey(VK_SPACE);
 						ReleaseKey(VK_SPACE);
 						winPressed = TRUE;
+
+					EnsureLanguageMode(SIMPLIFIED_CHINESE_SWITCH_METHOD_CTRL_SPACE);
 					}
 				}
 			}
@@ -228,4 +247,199 @@ LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 	}
 
 	return CallNextHookEx(hHook, nCode, wParam, lParam);
+}
+
+/*
+void EnsureLanguageMode(int simplified_chinese_flag)
+{
+	HKL hkl = GetKeyboardLayout(0);
+    DWORD dwLayout = (DWORD)(UINT_PTR)hkl & 0xFFFFFFFF;
+	//check https://learn.microsoft.com/en-us/windows-hardware/manufacture/desktop/windows-language-pack-default-values?view=windows-11 for full list of keyboard identifier
+
+
+//#if _DEBUG
+	printf("Current keyboard layout: %s\n", layoutName);
+//#endif
+	//Simplified Chinese
+  	if (dwLayout == 0x08040804 || dwLayout == 0x00000804)
+    {
+#if _DEBUG
+        printf("Simplified Chinese layout detected.\n");
+#endif
+
+		HWND hWnd = GetForegroundWindow();
+		if (hWnd == NULL)
+		{
+#if _DEBUG
+			printf("Failed to get foreground window. \n");
+#endif		
+			return;
+		}
+
+		HIMC hIMC = ImmGetContext(hWnd);
+        if (hIMC == NULL)
+        {
+#if _DEBUG
+            printf("Failed to get input context.\n");
+#endif
+            return;
+        }
+
+		DWORD fdwConversion = 0;
+        DWORD fdwSentence = 0;
+
+		        // 获取转换状态
+        if (ImmGetConversionStatus(hIMC, &fdwConversion, &fdwSentence))
+        {
+#if _DEBUG
+			printf("Current conversion mode: 0x%08X\n", fdwConversion);
+#endif
+            // 判断是否为中文输入模式
+            if (!(fdwConversion & IME_CMODE_NATIVE))
+            {
+    #if _DEBUG
+                printf("Current mode is English, switching to Chinese mode.\n");
+    #endif
+				if (simplified_chinese_flag == SIMPLIFIED_CHINESE_SWITCH_METHOD_SHIFT)
+				{
+					PressKey(VK_SHIFT);
+					ReleaseKey(VK_SHIFT);
+				} 
+				else if (simplified_chinese_flag == SIMPLIFIED_CHINESE_SWITCH_METHOD_CTRL)
+				{
+					PressKey(VK_CONTROL);
+					ReleaseKey(VK_CONTROL);
+				}
+
+				else if (simplified_chinese_flag == SIMPLIFIED_CHINESE_SWITCH_METHOD_CTRL_SPACE)
+				{
+		//#if _DEBUG
+					printf("Switching language mode using Ctrl + Space method.\n");
+		//#endif
+					PressKey(VK_CONTROL);
+					PressKey(VK_SPACE);
+					ReleaseKey(VK_SPACE);
+					ReleaseKey(VK_CONTROL);
+				}
+
+				else
+				{
+					printf("Unsupported switch method.\n");
+				}
+			}
+	
+			else
+			{
+				printf("Unsupported layout: %s\n", layoutName);
+			}
+			
+			ImmReleaseContext(hWnd, hIMC);
+		}
+
+		else
+		{
+			printf("Current layout is not Simplified Chinese.\n");
+		}
+	}
+}
+*/
+
+void EnsureLanguageMode(int simplified_chinese_flag)
+{
+    // 获取前台窗口句柄
+    HWND hWnd = GetForegroundWindow();
+    if (hWnd == NULL)
+    {
+    #ifdef _DEBUG
+        printf("Failed to get foreground window.\n");
+    #endif
+        return;
+    }
+
+    // 获取前台窗口所属的线程 ID
+    DWORD dwThreadId = GetWindowThreadProcessId(hWnd, NULL);
+
+    // 将当前线程附加到前台窗口的线程
+    DWORD dwCurrentThreadId = GetCurrentThreadId();
+    BOOL bAttached = AttachThreadInput(dwCurrentThreadId, dwThreadId, TRUE);
+
+    // 获取前台线程的键盘布局
+    HKL hkl = GetKeyboardLayout(dwThreadId);
+    DWORD dwLayout = (DWORD)(UINT_PTR)hkl & 0xFFFFFFFF;
+
+    char layoutName[KL_NAMELENGTH];
+    snprintf(layoutName, KL_NAMELENGTH, "%08X", dwLayout);
+
+#ifdef _DEBUG
+    printf("Current keyboard layout: %s\n", layoutName);
+#endif
+
+    // 判断是否为简体中文输入法
+    if (dwLayout == 0x08040804 || dwLayout == 0x00000804)
+    {
+    #ifdef _DEBUG
+        printf("Simplified Chinese layout detected.\n");
+    #endif
+
+        HIMC hIMC = ImmGetContext(hWnd);
+        if (hIMC == NULL)
+        {
+    #ifdef _DEBUG
+            printf("Failed to get input context.\n");
+    #endif
+            // 解除线程附加
+            if (bAttached)
+                AttachThreadInput(dwCurrentThreadId, dwThreadId, FALSE);
+            return;
+        }
+
+        DWORD fdwConversion = 0;
+        DWORD fdwSentence = 0;
+
+        // 获取转换状态
+        if (ImmGetConversionStatus(hIMC, &fdwConversion, &fdwSentence))
+        {
+    #ifdef _DEBUG
+            printf("Current conversion mode: 0x%08X\n", fdwConversion);
+    #endif
+            // 判断是否为中文输入模式
+            if (!(fdwConversion & IME_CMODE_NATIVE))
+            {
+    #ifdef _DEBUG
+                printf("Current mode is English, switching to Chinese mode.\n");
+    #endif
+                // 切换到中文输入模式
+                // 发送 Ctrl+Space 按键组合
+                PressKey(VK_CONTROL);
+                PressKey(VK_SPACE);
+                ReleaseKey(VK_SPACE);
+                ReleaseKey(VK_CONTROL);
+            }
+            else
+            {
+    #ifdef _DEBUG
+                printf("Already in Chinese input mode.\n");
+    #endif
+            }
+        }
+        else
+        {
+    #ifdef _DEBUG
+            printf("Failed to get conversion status.\n");
+    #endif
+        }
+
+        // 释放输入法上下文句柄
+        ImmReleaseContext(hWnd, hIMC);
+    }
+    else
+    {
+    #ifdef _DEBUG
+        printf("Current layout is not Simplified Chinese.\n");
+    #endif
+    }
+
+    // 解除线程附加
+    if (bAttached)
+        AttachThreadInput(dwCurrentThreadId, dwThreadId, FALSE);
 }
